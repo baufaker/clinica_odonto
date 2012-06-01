c84ca75f2a2a5e0f906375bfa3dbcadae8d4341d
Guilherme Baufaker <baufaker@gmail.com>
2012-06-01 12:30:21 -0300

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClinicaOdonto::Application.config.secret_token = '40feb7c4306ae876f0350db1ecdf5a008b10178c9d391441815267ba60dc32513afa64e5732b0c541210c354b0f1c3d788b67e1ec4005c2d2417bb201da455df'
